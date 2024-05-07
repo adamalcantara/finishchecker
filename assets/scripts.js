de4116d2dd0126681fbf033c1655dc57d5bbed17
adamalcantara <alcantaradevelopment@gmail.com>
2024-05-07 18:52:06 -0400

    Organizing the code

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,16 @@
+// Get HTML elements
 const input = document.getElementById("inputField");
 const button = document.getElementById("submitBtn");
 
+// Function to get the zip code from the HTML input
 button.onclick = function getZip() { 
     let zipCode = input.value;
     console.log(zipCode)
     getWeather(zipCode);
+    input.value = "";
 }
 
+// Function to get the weather statistics
 function getWeather(zipCode) {
     console.log("This is the test: " + zipCode);
     let URL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey;
