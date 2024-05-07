76b917dd0c7ecf8c535af0b548e7e27d4709e5ad
adamalcantara <alcantaradevelopment@gmail.com>
2024-05-06 20:37:20 -0400

    Code now uses user input zip code for weather pull

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,21 @@
-let zip = "34787";
+const input = document.getElementById("inputField");
+const button = document.getElementById("submitBtn");
 
+button.onclick = function getZip() { 
+    let zipCode = input.value;
+    console.log(zipCode)
+    getWeather(zipCode);
+}
 
-let URL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&appid=" + apiKey;
-
-fetch(URL)
-    .then(function (response) {
-        return response.json()
-    }).then(function (data) {
-        console.log(data);
-        console.log("This is the humidity: " + data.main.humidity + "%");
-    });
+function getWeather(zipCode) {
+    console.log("This is the test: " + zipCode);
+    let URL = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey;
+    
+    fetch(URL)
+        .then(function (response) {
+            return response.json()
+        }).then(function (data) {
+            console.log(data);
+            console.log("This is the humidity: " + data.main.humidity + "%");
+        });
+}
